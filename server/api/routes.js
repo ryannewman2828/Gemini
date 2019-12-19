f96a77c3186caabe5c9877e1aa62ecd03954133d
ryannewman2828 <ryannewman2828@gmail.com>
2019-12-19 15:04:10 -0500

    scrapped to go back to experimentation stage

--- server/api/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-// Testing Routes
-router.get('/v1/ping', (req, res) => res.json({ message: 'pong' }));
-
-module.exports = router;
